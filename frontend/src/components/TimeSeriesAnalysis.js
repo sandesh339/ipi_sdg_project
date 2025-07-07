bfc6692c571429117114ab4edd6f6b8e5419f09e
Sandesh Sharma Dulal <88919297+sandesh339@users.noreply.github.com>
2025-07-06 22:11:33 -0400

    Update TimeSeriesAnalysis.js

--- frontend/src/components/TimeSeriesAnalysis.js
+++ frontend/src/components/TimeSeriesAnalysis.js
@@ -25,7 +25,7 @@ ChartJS.register(
   Legend
 );
 
-const MAPBOX_TOKEN = "pk.eyJ1Ijoic2FuZGVzaDMzOSIsImEiOiJjbThqazJuaTYwaTlwMmtwdzU4NzUwN3YwIn0.Kc6gPcC0Jf2rSJN4ieeimA";
+const MAPBOX_TOKEN = process.env.MAPBOX_TOKEN;
 
 // Colors for different trend directions
 const TREND_COLORS = {
